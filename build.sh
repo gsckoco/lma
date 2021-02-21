6b82fedf5236de9b622191570cb0ed9022a0de91
Ben Johnston <gsckmp4@outlook.com>
2021-02-21 22:29:10 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-if g++ -std=c++14 `sourcefiles ./src c h cpp hpp` -o./out/lmc; then
+if g++-10 -std=c++14 -pipe `pkg-config --cflags gtkmm-3.0` `pkg-config --cflags --libs fmt` `sourcefiles ./src c cpp` -o./out/lmc `pkg-config --libs fmt` `pkg-config --libs gtkmm-3.0`; then
     ./out/lmc
 else
     echo "Unable to run, build failed"
