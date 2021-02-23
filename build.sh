a2a605526b60deafea552942128fd11ff7596ee6
Ben Johnston <gsckmp4@outlook.com>
2021-02-23 13:06:12 +0000

    Updated build.sh to passthrough arguments and add debug links

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-if g++-10 -std=c++14 -pipe `pkg-config --cflags gtkmm-3.0` `pkg-config --cflags --libs fmt` `sourcefiles ./src c cpp` -o./out/lmc `pkg-config --libs fmt` `pkg-config --libs gtkmm-3.0`; then
-    ./out/lmc
+if g++-10 -std=c++14 -g -pipe `pkg-config --cflags gtkmm-3.0` `pkg-config --cflags --libs fmt` `sourcefiles ./src c cpp` -o./out/lmc `pkg-config --libs fmt` `pkg-config --libs gtkmm-3.0`; then
+    ./out/lmc $@
 else
     echo "Unable to run, build failed"
 fi
